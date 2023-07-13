864fab300343ce4528ebe7d522dc336c995c28e5
Romina Echevarrieta <rechevar@directvla.com.ar>
2023-07-13 12:41:14 -0300

    configurando estilos

--- spotify/src/app/app.component.ts
+++ spotify/src/app/app.component.ts
@@ -7,31 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title='';
-  name:string='Romina'
-  age: number = 35
-  prhone: null = null
-  phone1: undefined = undefined
-  car: CarModel ={
-    brand:'Ford',
-    model:'Focus',
-    year: 2021
-  }
-  listCars: Array<CarModel> =[
-    {
-      brand:'Chevrolet',
-      model:'Camaro',
-      year: 2021
-    },
-    {
-      brand:'BMW',
-      model:'AMG 200',
-      year: 2021
-    }
-  ]    
-  
-}
-interface CarModel{
-  brand:string;
-  model:string;
-  year:number;
 }

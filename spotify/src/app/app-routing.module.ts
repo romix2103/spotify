3984ee6cf88956a65ca6d6f22b000dca982b400d
Romina Echevarrieta <rechevar@directvla.com.ar>
2023-07-14 10:24:08 -0300

    modularizando

--- spotify/src/app/app-routing.module.ts
+++ spotify/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExampleComponent } from './example/example.component';
 
 const routes: Routes = [
 {
-  path:''
+  path:'',
+  component: ExampleComponent
 }
 
 ];

5b4a5ff62a62137e74dffc0c9e2b91a46b18c460
Romina Echevarrieta <rechevar@directvla.com.ar>
2023-07-18 10:58:05 -0300

    agregando menu

--- spotify/src/app/shared/components/sidebar/sidebar.component.ts
+++ spotify/src/app/shared/components/sidebar/sidebar.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit{
+ linksMenu: Array<any> = [
+  {name: ''}
+ ]
+  mainMenu: {defaultOptions: Array<any>, accessLink: Array<any>}=
+  {defaultOptions: [], accessLink:[]}
+  constructor(){}
+  ngOnInit(): void{
+    this.mainMenu.defaultOptions = [
+      {
+        name: 'Home',
+        icon: 'uil uil-estate',
+        router: ['/', 'auth']
+      },
+      {
+        name: 'Buscar',
+        icon: 'uil uil-search',
+        router: ['/', 'history']
+      },
+      {
+        name: 'Tu biblioteca',
+        icon: 'uil uil-chart',
+        router: ['/', 'favorites'],
+        query: { hola: 'mundo' }
+      }
+    ]
+
+    this.mainMenu.accessLink = [
+      {
+        name: 'Crear lista',
+        icon: 'uil-plus-square'
+      },
+      {
+        name: 'Canciones que te gustan',
+        icon: 'uil-heart-medical'
+      }
+    ]
+
+  }
 
 }

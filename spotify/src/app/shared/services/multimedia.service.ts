844b1a4d48ebb07510b2ae0d5ab7ad1af6e4501e
Romina Echevarrieta <rechevar@directvla.com.ar>
2023-07-31 18:30:12 -0300

    rxjs observable

--- spotify/src/app/shared/services/multimedia.service.ts
+++ spotify/src/app/shared/services/multimedia.service.ts
@@ -1,9 +1,30 @@
 import { EventEmitter, Injectable } from '@angular/core';
+import { Observable, Observer } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MultimediaService {
   callback: EventEmitter<any> = new EventEmitter<any>()
-  constructor() { }
+ myObservable1$: Observable<any> = new Observable()
+  constructor() {
+    this.myObservable1$ = new Observable(
+      (observer: Observer<any>) =>{
+        //observer.next('ok observer')
+        //se completo, el resto de los eventos se suspenden
+        setTimeout(() =>{
+          observer.complete()
+        }, 1500)
+
+        setTimeout(() =>{
+          observer.next("ok")
+        }, 2500)
+
+        //el flujo deja de funcionar
+        setTimeout(() =>{
+          observer.error("ok")
+        }, 3500)
+      }
+    )
+   }
 }

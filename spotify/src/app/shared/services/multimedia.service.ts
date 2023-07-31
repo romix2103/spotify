7bce11aa27e2f34326febf3612335f07759478d7
Romina Echevarrieta <rechevar@directvla.com.ar>
2023-07-31 18:42:52 -0300

    fin seccion 9

--- spotify/src/app/shared/services/multimedia.service.ts
+++ spotify/src/app/shared/services/multimedia.service.ts
@@ -1,30 +1,31 @@
 import { EventEmitter, Injectable } from '@angular/core';
-import { Observable, Observer } from 'rxjs';
+import { BehaviorSubject, Observable, Observer, Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MultimediaService {
   callback: EventEmitter<any> = new EventEmitter<any>()
- myObservable1$: Observable<any> = new Observable()
+ myObservable1$: BehaviorSubject<any> = new BehaviorSubject('ok')
   constructor() {
-    this.myObservable1$ = new Observable(
-      (observer: Observer<any>) =>{
-        //observer.next('ok observer')
-        //se completo, el resto de los eventos se suspenden
-        setTimeout(() =>{
-          observer.complete()
-        }, 1500)
+    this.myObservable1$.next("ok")
+    // this.myObservable1$ = new Observable(
+    //   (observer: Observer<any>) =>{
+    //     // //observer.next('ok observer')
+    //     // //se completo, el resto de los eventos se suspenden
+    //     // setTimeout(() =>{
+    //     //   observer.complete()
+    //     // }, 1500)
 
-        setTimeout(() =>{
-          observer.next("ok")
-        }, 2500)
+    //     // setTimeout(() =>{
+    //     //   observer.next("ok")
+    //     // }, 2500)
 
-        //el flujo deja de funcionar
-        setTimeout(() =>{
-          observer.error("ok")
-        }, 3500)
-      }
-    )
+    //     // //el flujo deja de funcionar
+    //     // setTimeout(() =>{
+    //     //   observer.error("ok")
+    //     // }, 3500)
+    //   }
+    //)
    }
 }
